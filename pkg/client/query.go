45779b0fb5eadc7df27d6914be5a5af171420d0e
juricake <jurica.kenda@axilis.com>
2024-01-12 00:34:57 +0100

    remove query depagination because of result overriding

--- pkg/client/query.go
+++ pkg/client/query.go
@@ -10,49 +10,48 @@ import (
 	"github.com/juricaKenda/dynamodb.client/pkg/client/consts"
 )
 
-type QueryHelper struct {
-	client *Client
+type QueryIterator struct {
+	client   *Client
+	query    *dynamodb.QueryInput
+	firstRun bool
 }
 
 // Query method
-func (c *Client) Query(values interface{}, PK string, skCondition SortKeyCondition, SK string) error {
-	helper := newHelper(c)
-	req, err := helper.buildReq(PK, skCondition, SK)
+func (c *Client) Query(PK string, skCondition SortKeyCondition, SK string) (*QueryIterator, error) {
+	iterator, err := newIterator(c, PK, skCondition, SK)
+	if err != nil {
+		return nil, err
+	}
+
+	return iterator, nil
+}
+
+func newIterator(client *Client, PK string, skCondition SortKeyCondition, SK string) (*QueryIterator, error) {
+	q := &QueryIterator{client: client}
+	return q, q.buildReq(PK, skCondition, SK)
+}
+
+func (q *QueryIterator) HasNext() bool {
+	return q.firstRun || q.query.ExclusiveStartKey == nil
+}
+
+func (q *QueryIterator) Next(values interface{}) error {
+	q.firstRun = false
+	lastEvaluated, err := q.execQuery(values)
 	if err != nil {
 		return err
 	}
 
-	return helper.depaginate(values, req)
-}
-
-func newHelper(client *Client) *QueryHelper {
-	return &QueryHelper{
-		client: client,
-	}
-}
-
-func (q *QueryHelper) depaginate(values interface{}, req *dynamodb.QueryInput) error {
-	lastEvaluated, err := q.nextQuery(values, req)
-	if err != nil {
-		return err
-	}
-
-	for lastEvaluated != nil {
-		req.ExclusiveStartKey = lastEvaluated
-		lastEvaluated, err = q.nextQuery(values, req)
-		if err != nil {
-			return err
-		}
-	}
+	q.query.ExclusiveStartKey = lastEvaluated
 	return nil
 }
 
-func (q *QueryHelper) nextQuery(values interface{}, req *dynamodb.QueryInput) (map[string]*dynamodb.AttributeValue, error) {
+func (q *QueryIterator) execQuery(values interface{}) (map[string]*dynamodb.AttributeValue, error) {
 	if values == nil {
 		return nil, errors.New("given pointer to output values was nil")
 	}
 
-	result, err := q.client.dynamo.Query(req)
+	result, err := q.client.dynamo.Query(q.query)
 	if err != nil {
 		return nil, errors.New("failed to query Dynamo")
 	}
@@ -71,29 +70,30 @@ func (q *QueryHelper) nextQuery(values interface{}, req *dynamodb.QueryInput) (m
 	return result.LastEvaluatedKey, nil
 }
 
-func (q *QueryHelper) buildReq(PK string, skCondition SortKeyCondition, SK string) (*dynamodb.QueryInput, error) {
+func (q *QueryIterator) buildReq(PK string, skCondition SortKeyCondition, SK string) error {
 	exp := expression.NewBuilder()
 	keyCondition := expression.Key(consts.PK).Equal(expression.Value(PK))
 	switch skCondition {
 	case BeginsWith:
 		keyCondition = keyCondition.And(expression.Key(consts.SK).BeginsWith(SK))
 	default:
-		return nil, errors.New("unimplemented sk condition type")
+		return errors.New("unimplemented sk condition type")
 	}
 
 	exp = exp.WithKeyCondition(keyCondition)
 	spec, err := exp.Build()
 	if err != nil {
-		return nil, errors.New("building expression")
+		return errors.New("building expression")
 	}
 
-	return &dynamodb.QueryInput{
+	q.query = &dynamodb.QueryInput{
 		TableName: aws.String(q.client.table),
 
 		ExpressionAttributeNames:  spec.Names(),
 		ExpressionAttributeValues: spec.Values(),
 		KeyConditionExpression:    spec.KeyCondition(),
-	}, nil
+	}
+	return nil
 }
 
 type SortKeyCondition string
