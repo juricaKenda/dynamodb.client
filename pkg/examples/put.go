427e1949d8c520dc57135ce5bea1ee6d6d3b83ee
juricake <jurica.kenda@axilis.com>
2024-02-25 10:49:55 +0100

    chore(examples): add more expressiveness

--- pkg/examples/put.go
+++ pkg/examples/put.go
@@ -9,14 +9,15 @@ func main() {
 	db, _ := client.New("table-name", "region", "endpoint", nil)
 
 	// simple put example
-	_ = db.Put("my-partition", "my-sort", Value{A: "a", B: "b"})
+	_ = db.Put("my-partition", "my-sort", Value{A: "a", B: 123, C: true})
 
 	// put with options
 	expireAt := time.Now().Add(time.Hour)
-	_ = db.Put("my-partition", "my-sort", Value{A: "a", B: "b"}, client.TTL(expireAt.Unix()))
+	_ = db.Put("my-partition", "my-sort", Value{A: "a", B: 213, C: false}, client.TTL(expireAt.Unix()))
 }
 
 type Value struct {
 	A string `dynamodbav:"A"`
-	B string `dynamodbav:"B"`
+	B int64  `dynamodbav:"B"`
+	C bool   `dynamodbav:"C"`
 }
